ea0d8bbdd38ada365211b1968446dfcd85ff4873
Impaler <impaler@obviam.net>
2016-01-22 15:27:07 +0000

    Added basic input processor to the Game that deals with Nemo

--- core/src/net/obviam/nemo/Game.kt
+++ core/src/net/obviam/nemo/Game.kt
@@ -2,6 +2,8 @@ package net.obviam.nemo
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
@@ -16,6 +18,7 @@ class Game : ApplicationAdapter() {
         batch = SpriteBatch()
         img = Texture("images/nemo_01.png")
         nemo = Nemo()
+        Gdx.input.inputProcessor = KeyInputProcessor()
     }
 
     override fun render() {
@@ -26,6 +29,30 @@ class Game : ApplicationAdapter() {
         batch.end()
     }
 
-    data class Nemo(val position: Vector2 = Vector2(10f, 10f))
+    data class Nemo(val position: Vector2 = Vector2(0f, 0f))
 
+    inner class KeyInputProcessor : InputAdapter() {
+
+        override fun keyDown(keycode: Int): Boolean {
+            when (keycode) {
+                Input.Keys.LEFT -> moveCharacter(Direction.LEFT)
+                Input.Keys.RIGHT -> moveCharacter(Direction.RIGHT)
+                else -> return false
+            }
+            return true
+        }
+    }
+
+    fun moveCharacter(direction: Direction) {
+        if (direction.equals(Direction.LEFT) && nemo.position.x >= 10) {
+            nemo.position.x -= 10
+        }
+        if (direction.equals(Direction.RIGHT) && nemo.position.x <= 300) {
+            nemo.position.x += 10
+        }
+    }
+}
+
+enum class Direction {
+    LEFT, RIGHT
 }
