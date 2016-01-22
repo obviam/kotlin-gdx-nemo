eb3d0e6808f99c0633b5b441b49666bc979b285b
Impaler <impaler@obviam.net>
2016-01-22 14:53:20 +0000

    Refactoring  * Renamed Nemo to Game  * Added Nemo as a data class

--- core/src/net/obviam/nemo/Game.kt
+++ core/src/net/obviam/nemo/Game.kt
@@ -5,21 +5,27 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
 
-class Nemo : ApplicationAdapter() {
+class Game : ApplicationAdapter() {
     internal lateinit var batch: SpriteBatch
     internal lateinit var img: Texture
+    internal lateinit var nemo: Nemo
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        img = Texture("images/nemo_01.png")
+        nemo = Nemo()
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        batch.draw(img, nemo.position.x, nemo.position.y)
         batch.end()
     }
+
+    data class Nemo(val position: Vector2 = Vector2(10f, 10f))
+
 }
